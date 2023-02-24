4982e5fea811f3e42dd1d9ca92bf927d4745da6b
Dixit <diks@gmail.com>
2023-02-24 11:43:29 -0400

    sdf

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.get('/', (req, res) => {
 
 
 const postStart = async () => {
+    console.log("shjdgfjhsgbfjhgasdfjhg ");
     const resp = await axios.post('http://52.91.127.198:8080/start', {
         banner: "B00913652",
         ip: "100.25.159.24:5000"
